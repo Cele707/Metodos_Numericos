7459ddd5ca580daa9107b71b9ed1f96f63b3a32f
Cele <celeste.nunez0601@gmail.com>
2024-08-22 21:57:33 -0300

    Secante nueva version

--- Secante_ejemplo.cpp
+++ Secante_ejemplo.cpp
@@ -4,7 +4,7 @@
 //es utilizado si no puedo obtener la derivada a mano
 //funciones
 double funcion (double);
-double secante (double, double);
+void secante (double, double);
 
 int main(int argc, char *argv[]) {
 	//declaracion de variables
@@ -21,18 +21,21 @@ int main(int argc, char *argv[]) {
 double funcion (double x){
 	return exp(x)-pow(x, 2);
 }
-
-void newton_raphson (double x_viejo, double x_vv){
+	
+void secante (double x_viejo, double x_vv){
 	double tol=10*exp(-10);
 	double error, error_porcentual, x_nuevo;
 	int iteracion=0;
 	do{
 		iteracion++;
-		x_nuevo = (x_viejo - (funcion(x_viejo)*(x_vv - x_viejo)))/ (funcion(x_vv)-funcion(x_viejo));
-		error=abs(x_nuevo-x_viejo);
-		error_porcentual=((x_nuevo - x_viejo)/x_viejo)*100;
+		x_nuevo = x_viejo - (funcion(x_viejo)*(x_vv - x_viejo))/ (funcion(x_vv)-funcion(x_viejo));
+		
+		error=fabs(x_nuevo-x_viejo);
+		error_porcentual=((x_nuevo - x_viejo)/ x_viejo)*100;
+		x_vv = x_viejo;
 		x_viejo=x_nuevo;
+		
 	} while(error > tol && iteracion<=100000);
-		printf("\nLa raiz es %lf\nEl error aproximado es %lf y el porcentual es %lf\nLa cantidad de iteraciones realizadas es de %d",x_viejo, error, error_porcentual, iteracion);
+		printf("\nLa raiz es %.10lf\nEl error aproximado es %.10lf y el porcentual es %.2lf\nLa cantidad de iteraciones realizadas es de %d",x_viejo, error, error_porcentual, iteracion);
 }
 			
