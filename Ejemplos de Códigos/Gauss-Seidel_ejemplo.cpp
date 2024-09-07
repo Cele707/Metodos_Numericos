e74045d1f342ee8ec1bbdc6aab70b876e0e189a0
cnunez976 <cnunez976@alumnos.iua.edu.ar>
2024-09-07 18:28:53 -0300

    Se agregó el método de Gauss-Seidel y se terminó de refinar el método de Jacobi

--- Ejemplos de Códigos/Gauss-Seidel_ejemplo.cpp
+++ Ejemplos de Códigos/Gauss-Seidel_ejemplo.cpp
@@ -3,22 +3,21 @@
 #include <math.h>
 #include <stdio.h>
 #define n 3 //definimos tamanio matriz
-/*consiste en despejar cada incógnita de un sistema de ecuaciones lineales y resolverlas iterativamente. 
-Para cada ecuación, despejamos la incógnita actual usando los valores de las demás incógnitas,
- que son aproximaciones de la iteración anterior.*/
-
 using namespace std;
+/*Cada componente de xx se actualiza inmediatamente en la misma iteración, 
+lo que significa que los valores recién calculados de la iteración actual 
+se utilizan de inmediato para calcular el siguiente valor.*/
 
 //funciones
 void cargar(double A[n][n], double B[n]);
 void imprimir(double A[n][n], double B[n]);
-void jacobi(double A[n][n], double B[n]);
+void Gauss_Seidel(double A[n][n], double B[n]);
 
 int main(int argc, char *argv[]) {
 	double A [n][n], B[n];
 	int op_menu=0;
 	do{
-		printf("METODO ITERATIVO DE JACOBI\n");
+		printf("METODO ITERATIVO DE GAUSS-SEIDEL\n");
 		printf("1-Cargar datos\n");
 		printf("2-salir\n");
 		printf("Ingresar elecccion:");
@@ -28,7 +27,7 @@ int main(int argc, char *argv[]) {
 		case 1:
 			cargar(A, B);
 			imprimir(A,B);
-			jacobi(A, B);
+			Gauss_Seidel(A, B);
 			printf("\n");
 			break;
 		case 2:
@@ -54,79 +53,91 @@ void cargar(double A[n][n], double B[n]){
 		scanf("%lf", &B[i]);
 	}
 }
-	void imprimir (double A[n][n], double B[n]){
-		printf("\nLa matriz se ve de esta manera:\n");
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
-				printf("%.2lf \t", A[i][j]);
-			}
-			printf("| %.2lf\n", B[i]);
+	
+void imprimir (double A[n][n], double B[n]){
+	printf("\nLa matriz se ve de esta manera:\n");
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			printf("%.2lf \t", A[i][j]);
 		}
+		printf("| %.2lf\n", B[i]);
 	}
-void jacobi (double A[n][n], double B[n]){
-//declaracion de variables
+}
+	
+void Gauss_Seidel(double A[n][n], double B[n]){
+	//declaracion de variables
 	double Xv[n] = {0}; //soluciones anteriores
 	double Xn[n] = {0}; //solucoines nuevas
 	double tol=10*exp(-10); //tolerancia
 	double error=0, error_anterior=0, suma=0;
 	int iter=0;
-//inicio
+	//inicio
 	//inicializamos Xv por consola
 	printf("\nIngrese los valores iniciales para el vector X:\n");
-	for(int i=0; i<n; i++){
+		for(int i=0; i<n; i++){
 		printf("X[%d]:", i);
 		scanf("%lf", &Xv[i]);
-	}
+		}
 	//verificamos si es diagonalmente dominante (Q el valor abosluto de A[i][i] > valor absoluto de la Sumatoria de los demas numeros de la fila
 	for(int i=0; i<n; i++){
-		suma=0;
+	suma=0;
 		for(int j=0; j<n; j++){
 			if(j != i){
 				suma += fabs(A[i][j]);
 			}
 		}
 		if(fabs(A[i][i]) <= suma){
-			printf("\nLa matriz no es diagonalmene dominante, el método podría no converger\n");
+		printf("\nLa matriz no es diagonalmene dominante, el método podría no converger\n");
 		}
 	}
-	do{
+		do{
 		iter++;
-	//calculo de la siguiente iteracion, la siguiente incógnita
+	//iteracion para cada fila del sistema
 		for(int i=0; i<n; i++){
-			suma=0;
-			for(int j=0; j<n; j++){
-				if(j != i){
-					suma += A[i][j] * Xv[j];
-				}
+		suma=0; 
+		//caso especial para la primera fila
+		if(i==0){
+			//Sumar los productos de los elementos fuera de la diagonal (a la derecha)
+			for(int j=1; j<n; j++){
+				suma+=A[i][j]*Xv[j];
+			}
+		Xn[i]=(B[i]-suma)/(A[i][i]);
+		}
+		else{
+			// Sumar los productos de los elementos a la izquierda de la diagonal
+			for(int j=0; j<i; j++){
+				suma+=A[i][j]*Xn[j];
+			}
+			// Sumar los productos de los elementos a la derecha de la diagonal
+			for(int j=i+1; j<n; j++){
+				suma+=A[i][j]*Xv[j];
 			}
 			Xn[i]=(B[i]-suma)/(A[i][i]);
+			}
 		}
-		suma=0;
 	//calculo de error
+		suma=0;
 		for(int i=0; i<n; i++){
 			suma += pow((Xn[i]-Xv[i]), 2);//error cuadratico
 		}
 		error= sqrt(suma);//error total
 	//Verificamos convergencia-> Si el error está aumentando en lugar de disminuir (es decir, error > errort), el código da por hecho que el método no está convergiendo
 		if(iter >= 2 && error > error_anterior){
-			printf("\nEl método no converge");
-			exit(0);
+		printf("\nEl método no converge");
+		exit(0);
 		}
 		error_anterior=error;
 		for(int i=0; i<n; i++){
 			Xv[i]=Xn[i];
 		}
 	} while(error>tol && iter <= 100000);
-	
+		
 	printf("\nLas soluciones del sistema luego de %d iteraciones:\n\n", iter);
-	for(int i=0; i<n; i++){
-		printf("X[%d]: %.2lf\n", i, Xn[i]);
-	}
-	printf("\nEl error aproximado es %.10lf\n", error);
-	
+		for(int i=0; i<n; i++){
+			printf("X[%d]: %.2lf\n", i, Xn[i]);
+		}
+		printf("\nEl error aproximado es %.10lf\n", error);
 }
-
-
-
-
-
+			
+			
+			
