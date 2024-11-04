802ef5411efa99951f36b7a479521e41f22455c0
Cele :) <cnunez976@alumnos.iua.edu.ar>
2024-11-04 18:10:52 -0300

    Modificacion de los codigos de EDO y del Gauss-Legendre

--- Ejemplos de Códigos/2 parcial/EDO/Heun_ejemplo.cpp
+++ Ejemplos de Códigos/2 parcial/EDO/Heun_ejemplo.cpp
@@ -10,16 +10,11 @@ int main(int argc, char *argv[]) {
 	double x0 = 0.0;
 	double xf = 1.0;  // Inicializado a un valor arbitrario
 	double y0 = 1.0;
-	double h = 0.2;
-	double n = (xf-x0)/h; // Corregido: Definir número de pasos correctamente
+	double h = 0;
+	int n = 0; // Corregido: Definir número de pasos correctamente
 	double yt;
+	int op_menu=0;
 	
-	vector<double> x(n+1);
-	vector<double> y(n+1);
-	
-	// Inicialización de condiciones iniciales
-	x[0] = x0;
-	y[0] = y0;
 	
 	// Abrir el archivo para escritura
 	FILE *file = fopen("Heun_tabla.txt", "w");
@@ -30,8 +25,38 @@ int main(int argc, char *argv[]) {
 	// Escribir los valores iniciales en el archivo
 	fprintf(file, "%lf\t%lf\n", x0, y0);
 	
-	printf("Metodo de Punto Heun (Error de orden h³)\n");
+	printf("--Metodo de Punto Heun (Error de orden h³)--\n");
 	
+	do{
+		printf("\nDatos conocidos:\n1-n (cantidad de subintervalos)\n2-h (ancho del intervalo)\n3-Salir\nRespuesta: ");
+		scanf("%d", &op_menu);
+		switch(op_menu){
+		case 1:
+			printf("\nIngrese el valor del n:");
+			scanf("%d", &n);
+			h=(xf-x0)/n;
+			printf("El 'h' obtenido es de %lf\n", h);
+			break;
+		case 2:
+			printf("\nIngrese el valor del h:");
+			scanf("%lf", &h);
+			n=(xf-x0)/h;
+			printf("El 'n' obtenido es de %d\n", n);
+			break;
+			
+		case 3:
+			fclose(file);// Cerrar el archivo antes de salir
+			return 0;
+			break;
+		}
+	
+	//Redimencion luego de definir n
+	vector<double> x(n+1);
+	vector<double> y(n+1);
+	
+	// Inicialización de condiciones iniciales
+	x[0] = x0;
+	y[0] = y0;
 	// Método de Heun
 	printf("\nX0 = %lf \tY0 = %lf", x0, y0); // Se imprime el primer el x0 e y0
 	for(int i = 0; i < n; i++) {  // Cambié de n-1 a n para calcular todos los puntos
@@ -42,8 +67,11 @@ int main(int argc, char *argv[]) {
 		printf("\nX%d = %lf \tY%d = %lf", i+1, x[i+1], i+1, y[i+1]);
 		fprintf(file, "%lf\t%lf\n",x[i+1], y[i+1]);
 	}
-
 	return 0;
+	
+	}while(op_menu != 3);
+	
+	fclose(file);
 }
 
 
--- Ejemplos de Códigos/2 parcial/EDO/Runge-Kutta_ejemplo.cpp
+++ Ejemplos de Códigos/2 parcial/EDO/Runge-Kutta_ejemplo.cpp
@@ -8,14 +8,15 @@ double f(double x ,double y) {
 
 int main(int argc, char *argv[]) {
 	double x0 = 0.0;
-	double xf = 1.0;  // Inicializado a un valor arbitrario
+	double xf = 1.0; // Inicializado a un valor arbitrario
 	double y0 = 1.0;
 	double h = 0;
-	int n = 0;//Definir número de pasos correctamente
+	int n = 0;
+	double k1, k2, k3, k4;
 	int op_menu=0;
-	
+
 	// Abrir el archivo para escritura
-		FILE *file = fopen("Euler_tabla.txt", "w");
+	FILE *file = fopen("RK4_tabla.txt", "w");
 	if (file == NULL) {
 		printf("Error al abrir el archivo.\n");
 		return 1;
@@ -23,7 +24,7 @@ int main(int argc, char *argv[]) {
 	// Escribir los valores iniciales en el archivo
 	fprintf(file, "%lf\t%lf\n", x0, y0);
 	
-	printf("--Metodo de Euler (Error de orden h²)--\n");
+	printf("--Metodo de RK4 (Error de orden h^4)--\n");
 	do{
 		printf("\nDatos conocidos:\n1-n (cantidad de subintervalos)\n2-h (ancho del intervalo)\n3-Salir\nRespuesta: ");
 		scanf("%d", &op_menu);
@@ -39,14 +40,12 @@ int main(int argc, char *argv[]) {
 			scanf("%lf", &h);
 			n=(xf-x0)/h;
 			printf("El 'n' obtenido es de %d\n", n);
-			break;
-			
+		break;
 		case 3:
 			fclose(file);// Cerrar el archivo antes de salir
 			return 0;
 			break;
 		}
-		
 		//Redimensionar los vectores después de definir `n`
 		vector<double> x(n+1);
 		vector<double> y(n+1);
@@ -55,14 +54,22 @@ int main(int argc, char *argv[]) {
 		x[0] = x0;
 		y[0] = y0;
 		
-		printf("\nX0 = %lf \tY0 = %lf", x0, y0); // Se imprime el primer el x0 e y0
-		for(int i = 0; i < n; i++) { 
-			x[i+1] = x[i] + h;
-			y[i+1] = y[i] + h * f(x[i], y[i]);
+		printf("\nTabla de Datos obtenida:\n");
+		printf("X0 = %lf \tY0 = %lf", x0, y0); // Se imprime el primer el x0 e y0
+		for(int i = 0; i < n; i++) {  // Cambié de n-1 a n para calcular todos los puntos
+			k1 = f(x[i], y[i]);
+			k2 = f(x[i] + 0.5 * h, y[i] + 0.5 * h * k1);
+			k3 = f(x[i] + 0.5 * h, y[i] + 0.5 * h * k2);
+			k4 = f(x[i] + h, y[i] + h * k3);
+			
+			y[i + 1] = y[i] + (h / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4);
+			x[i + 1] = x[i] + h;
 			printf("\nX%d = %lf \tY%d = %lf", i+1, x[i+1], i+1, y[i+1]);
 			fprintf(file, "%lf\t%lf\n",x[i+1], y[i+1]);
 		}
 		return 0;
-	} while (op_menu != 3);
+		
+	} while(op_menu != 3);
 	fclose(file);
 }
+
